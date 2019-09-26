c799bd12e75b8a98f29ac47b0ccf3ee1df391375
installuser <installuser@NM355771.tw.sinyi.com>
2019-09-26 11:00:21 +0800

    react my-app

---
+++ pages/Hello/World.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../../components/nav'
+
+const World = () => (
+    <>
+        <h1>Hello World!</h1>
+        <span>Next.js</span>
+    </>
+)
+
+export default World
